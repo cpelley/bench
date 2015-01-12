45f228cb298b00372dcd4cfa319819ed01c81c47
Carwyn Pelley <cpelley.pub@gmail.com>
2015-01-12 16:35:18 +0000

    ENH: Simplification
    Conflicts:
    	lib/bench/__init__.py

--- lib/bench/__init__.py
+++ lib/bench/__init__.py
@@ -1,6 +1,6 @@
 # The MIT License (MIT)
 #
-# Copyright (c) 2014 Carwyn Pelley
+# Copyright (c) 2013 Carwyn Pelley
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -23,110 +23,17 @@
 Bench is a small module for determining program memory usage.
 
 '''
-from functools import wraps
 import os
-import sys
 
 
-class _ContextDecorator(object):
-    """
-    A modified base class or mixin taken from python3.2 contextlib source that
-    enables context managers to work as decorators.
+class Memory(object):
+    def __init__(self, vmsize, vmpeak, vmrss, vmhwm, tag=None):
+        self.mem = {'vmsize': vmsize, 'vmpeak': vmpeak, 'vmrss': vmrss,
+                    'vmhwm': vmhwm}
+        self.tag = tag
 
-    """
-    def __call__(self, func):
-        @wraps(func)
-        def inner(*args, **kwds):
-            with self:
-                return func(*args, **kwds)
-        return inner
-
-
-class _Engine(object):
-    fields = ['VmSize', 'VmRSS', 'VmPeak', 'VmHWM']
-
-    def get_log(self):
-        return NotImplemented
-
-
-class _LinuxEngine(_Engine):
-    def __init__(self):
-        self._pid = os.getpid()
-
-    @property
-    def pid(self):
-        return self._pid
-
-    def get_log(self):
-        def get_field(string):
-            stat = string.replace(' ', '').replace('\t', '').split(':')
-            if stat[0] in self.fields:
-                assert stat[1][-2:] == 'kB'
-                stat = {stat[0]: int(stat[1][:-2])}
-            else:
-                stat = None
-            return stat
-
-        fnme = os.path.join('/', 'proc', str(self.pid), 'status')
-        with open(fnme, 'r') as fh:
-            stat = fh.read()
-        fields = {}
-        for line in stat.split('\n'):
-            field = get_field(line)
-            if field:
-                fields.update(field)
-        return fields
-
-
-# Set operating system dependent engine.
-if os.name == 'posix':
-    ENGINE = _LinuxEngine()
-else:
-    raise RuntimeError('operating system: {} not yet supported'.format(
-        sys.platform))
-
-
-class MemoryUsage(_ContextDecorator):
-    """
-    Determine process current and peak usage as well as consumption between
-    retrievals.
-
-    """
-    def __init__(self, out='stdout'):
-        self._pid = os.getpid()
-        self._stat = []
-        self.get_log()
-        self._summary_print = True
-        self.out = out
-
-    @property
-    def usage(self):
-        return self._stat
-
-    @property
-    def pid(self):
-        return self._pid
-
-    def get_log(self):
-        fields = ENGINE.get_log()
-        self.usage.append(fields)
-
-    @staticmethod
-    def print_summary(statistic):
-        width = 9
-        msg = (''.join([item.rjust(width*2, ' ') for item in
-               ['Current Usage', 'Current Usage Inc', 'Process Peak',
-                'Process Peak Inc']])) + '\n'
-        msg = msg + ' '.ljust(width*2, '#') * 4 + '\n'
-        msg = msg + (''.join([item.rjust(width, ' ') for item in
-                     ['VirtMem', 'RSS']])) * 4 + '\n'
-        msg += '{}' * 8 + '\n'
-        params = [statistic['VmSize'], statistic['VmRSS'],
-                  statistic['inc_VmSize'], statistic['inc_VmRSS'],
-                  statistic['VmPeak'], statistic['VmHWM'],
-                  statistic['inc_VmPeak'], statistic['inc_VmHWM']]
-        for iparam in range(len(params)):
-            param = params[iparam]
+    def __str__(self):
+        def get_unit(param):
             if param > 1e9:
                 param *= 1e-9
                 unit = 'TB'
@@ -138,32 +45,45 @@ class MemoryUsage(_ContextDecorator):
                 unit = 'MB'
             else:
                 unit = 'KB'
-            params[iparam] = ('{:6.2f}{}'.format(param, unit))
-            params[iparam] = params[iparam].rjust(width, ' ')
+            return param, unit
 
-        print msg.format(*params)
+        mem = {}
+        for key, value in self.mem.iteritems():
+            val, unit = get_unit(value)
+            mem[key] = '{:6.2f} {}'.format(val, unit)
 
-    def get_usage(self):
-        self.get_log()
-        diff_log = {'inc_{}'.format(name):
-                    (self.usage[-1][name] - self.usage[-2][name]) for
-                    name in self.usage[-1].keys()}
-        diff_log.update({name: self.usage[-1][name] for name in
-                         self.usage[-1].keys()})
-        self.print_summary(diff_log)
+        return ('Virtual memory usage {} (peak {})\n'
+                'Resident set size {} (peak {})\n'
+                'Tag: {}\n\n'.format(
+                mem['vmsize'], mem['vmpeak'], mem['vmrss'], mem['vmhwm'],
+                self.tag))
 
-    def __enter__(self):
-        return self
+    def __sub__(self, other):
+        res = {}
+        for key, value in self.mem.iteritems():
+            res[key] = value - other.mem[key]
+        res['tag'] = '({} - {})'.format(self.tag, other.tag)
+        return Memory(**res)
 
-    def __exit__(self, type, value, traceback):
-        self.get_usage()
+
+def get_usage(tag=None):
+    fnme = os.path.join('/', 'proc', str(os.getpid()), 'status')
+    usage = {}
+    with open(fnme, 'r') as fh:
+        for line in fh:
+            key, value = line.split(':')
+            key = key.strip().lower()
+            if key in ['vmsize', 'vmpeak', 'vmrss', 'vmhwm']:
+                usage[key] = int(''.join([ss for ss in value.strip() if
+                                          ss.isdigit()]))
+            usage['tag'] = tag
+    return Memory(**usage)
 
 
 if __name__ == '__main__':
-    #import sys
+    import numpy as np
 
-    #module = os.path.splitext(sys.argv[1])[0]
-    mem = MemoryUsage()
-    #exec('import {}'.format(module))
-    a = range(300000)
-    mem.get_usage()
+    mem = get_usage('before array creation')
+    arr = np.arange(200000)
+    mem2 = get_usage('after array creation')
+    print mem2 - mem
